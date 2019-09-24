11cd58fc0997671fc8e3b896d7eb57f2aa814219
koljn12345 <litovchenko_kolj@mail.ru>
2019-09-24 14:02:29 +0300

    feat:add solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let code=[];
+    let arrayResultStrong=[];
+    let arrayStrong=expr.split("**********");
+    count=0;
+    for(let i=0;i<arrayStrong.length;i++) {
+        for(let j=0,count=0;j<arrayStrong[i].length;j++,count++) {
+            if(count==10) { arrayResultStrong+=MORSE_TABLE[code];code=[]; count=0;}
+            if(arrayStrong[i][j]==1 && arrayStrong[i][j+1]==0) {code+='.'; j++;count++;}
+            else if(arrayStrong[i][j]==1 && arrayStrong[i][j+1]==1) {code+='-'; j++;count++;}
+            if(j==arrayStrong[i].length-1) { arrayResultStrong+=MORSE_TABLE[code];code=[]; count=0;}
+            }
+        if(i!=arrayStrong.length-1) arrayResultStrong+=" ";
+            
+    }
+    return(arrayResultStrong);
 }
 
 module.exports = {
